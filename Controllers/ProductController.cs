cc9973b9ac97e10cfc8fb5bb796ab9d6f2ff16a6
Mark875 <vasilevskiy.mark@gmail.com>
2022-11-27 19:46:55 +0300

    Initial commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopM4.Data;
+using ShopM4.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShopM4.Models.ViewModels;
+
+namespace ShopM4.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        // GET INDEX
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objList = db.Product;
+
+            foreach (var item in objList)
+            {
+                item.Category = db.Category.FirstOrDefault(x => x.Id == item.CategoryId);
+            }
+
+            return View(objList);
+        }
+
+        // GET - CreateEdit
+        public IActionResult CreateEdit(int? id)
+        {
+            /*
+            IEnumerable<SelectListItem> CategoriesList = db.Category.Select(x => 
+            new SelectListItem()
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            });
+
+            //ViewBag.CategoriesList = CategoriesList;
+            ViewData["CategoriesList"] = CategoriesList; */
+
+            ProductViewModel productViewModel = new ProductViewModel()
+            {
+                Product = new Product(),
+                CategoriesList = db.Category.Select(x =>
+                new SelectListItem()
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                })
+            };
+            if (id == null)
+            {
+                // create product
+                return View(productViewModel);
+            }
+            else
+            {
+                // edit product
+                productViewModel.Product = db.Product.Find(id);
+                if (productViewModel.Product == null)
+                {
+                    return NotFound();
+                }
+                return View(productViewModel.Product);
+            }
+        }
+    }
+}
+
---
+++ Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+
+namespace ShopM4.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Product Product { get; set; }
+        public IEnumerable<SelectListItem> CategoriesList { get; set; }
+    }
+}
